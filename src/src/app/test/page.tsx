8e69055626896117704674fef91df597efc4bb52
panadol94 <79234689+panadol94@users.noreply.github.com>
2026-03-18 16:42:04 +0800

    Add test page src/src

---
+++ src/src/app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: 50, textAlign: 'center' }}>
+      <h1>Test Page</h1>
+      <p>If you see this, the page works!</p>
+    </div>
+  )
+}
